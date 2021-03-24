63bddd11bb06d7dde61f81bd9d30fc2cd6e4545c
Justin Raver <justinraver113@gmail.com>
2021-03-24 10:53:37 -0600

    exercise 1-17

--- KandR/chap1/exercises/1-17/printLongerThan80.c
+++ KandR/chap1/exercises/1-17/printLongerThan80.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-
+void printOver80();
 
 int main(){
+	printOver80();
+	return 0;
+}
 
 
+void printOver80(){
+	char c[1000];
+	
+	int i=0;
+	while((c[i] = getchar()) != EOF){
+		if(c[i] == '\n'){
+			if(i>80){
+				c[i+1] = '\0';
+				printf("%s",c);
+			}
+			i = 0;
+		}else{
+			i++;
+		}
+	}
 }
